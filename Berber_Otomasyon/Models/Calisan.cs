77234f77875a9335c9da931e0c1129cb162be99f
talhairis <talhairiss@gmail.com>
2024-12-10 13:21:31 +0300

    Proje dosyası ekle.

---
+++ Berber_Otomasyon/Models/Calisan.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Berber_Otomasyon.Models
+{
+	public class Calisan
+	{
+		[Key]
+		private long calisanId;
+
+		[Required]
+		[MaxLength(30)]
+		[Display(Name = "Calışan Adı")]
+		private string calisanAd;
+
+		[Required]
+		[MaxLength(30)]
+		[Display(Name = "Calışan Soyadı")]
+		private string calisanSoyad;
+
+		[Required]
+		[EmailAddress(ErrorMessage = "Calışanın e-postasını uygun formatta giriniz!")]
+		[Display(Name = "Calışan E-postası")]
+		private string calisanMail;
+
+		public ICollection<CalisanRandevu>? CalisanRandevulari { get; set; }
+
+
+	}
+}
