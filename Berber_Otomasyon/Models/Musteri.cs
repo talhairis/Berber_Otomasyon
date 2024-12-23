e13752306db16e6f730e636cb1481ca1c4ab1e79
talhairis <talhairiss@gmail.com>
2024-12-23 23:18:06 +0300

    Musteri tablosunu ekle

---
+++ Berber_Otomasyon/Models/Musteri.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Berber_Otomasyon.Models
+{
+	public class Musteri : IdentityUser
+	{
+
+		[Display(Name = "Müşteri Adı")]
+		[Required(ErrorMessage = "Lütfen müşteri adını giriniz!")]
+		[StringLength(50, ErrorMessage = "Lütfen müşteri adını 50 karakterden fazla girmeyiniz!")]
+		private string musteriAd;
+
+		[Display(Name = "Müşteri Soyadı")]
+		[Required(ErrorMessage = "Lütfen müşteri soyadını giriniz!")]
+		[StringLength(50, ErrorMessage = "Lütfen müşteri soyadını 50 karakterden fazla girmeyiniz!")]
+		private string musteriSoyad;
+
+		[Display(Name = "Müşteri E-Maili")]
+		[Required(ErrorMessage = "Lütfen müşteri e-maili giriniz!")]
+		[EmailAddress(ErrorMessage = "Lütfen e-mail adresini doğru formatta giriniz!")]
+		private string musteriMail;
+
+	}
+}
